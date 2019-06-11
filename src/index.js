03e147bcf20a0d65259102f44c9dafc28107127c
Patric França <patric.franca@hubfintech.com.br>
2019-06-11 16:57:39 -0300

    incluindo upload de imagens

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
 
-require('./app/controllers/index')(app);
+require("./app/controllers/index")(app);
 
 app.listen(3000);
